7011316fb41f4072fbed81e651ac9106edf8ec8c
Giorgiopri22 <giorgioprizzi@gmail.com>
2023-05-02 14:44:10 +0200

    Fine esr

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+
+    export default{
+        name:"AppTitle",
+       data(){
+        return{
+            
+        }
+
+       }       
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+</template>
